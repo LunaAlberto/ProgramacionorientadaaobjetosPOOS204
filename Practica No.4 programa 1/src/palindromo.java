4ca961d8808be1a39c5cee2a5b88dac0b861f160
Alberto Luna <123046909@UPQ.EDU.MX>
2025-01-28 16:46:22 -0600

    finalizo trabajo

--- Practica No.4 programa 1/src/palindromo.java
+++ Practica No.4 programa 1/src/palindromo.java
@@ -1,2 +1,31 @@
+
+import java.util.*;
+import java.util.Scanner;
 public class palindromo {
+    public static void main(String[] args) {
+
+        /*
+4. Codifica un programa que solicite una cadena de caracteres e imprima como
+resultado si la cadena es palíndromo o no
+         */
+
+        System.out.println("Por alberto luna");
+        Scanner ln = new Scanner(System.in);
+
+        System.out.println("Ingrese su frase de palindromo");
+        String pa = ln.nextLine();
+
+String palabrainvertida = new StringBuilder(pa).reverse().toString();
+
+if(palabrainvertida.equalsIgnoreCase(pa)){
+    System.out.println("es palindromo");
+
+
+}else {
+    System.out.println("no es palindromo");
+}
+
+
+
+    }
 }
