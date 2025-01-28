b91943c9aa96082fc5aa48d9d1423d61892ac3c5
Alberto Luna <123046909@UPQ.EDU.MX>
2025-01-27 23:49:34 -0600

    se creó proyecto 6

---
+++ 6to proyecto/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    /*
+    6. Crea un programa que pida al usuario que introduzca una frase en y muestre
+en pantalla la frase invertida.
+     */
+    public static void main(String[] args) {
+        System.out.println("Por alberto luna");
+
+        Scanner lu = new Scanner(System.in);
+
+        System.out.println("Introduzca cual quier frase:");
+        String frase = lu.nextLine();
+
+        
+
+
+    }
+}
