723d6611409f842c9c4cdc8b81f1c82c4c7519fb
Alberto Luna <123046909@UPQ.EDU.MX>
2025-01-27 23:55:23 -0600

    se finalizo proyecto 6

--- 6to proyecto/src/Main.java
+++ 6to proyecto/src/Main.java
@@ -5,15 +5,20 @@ public class Main {
 en pantalla la frase invertida.
      */
     public static void main(String[] args) {
+
+        String s2="";
         System.out.println("Por alberto luna");
 
         Scanner lu = new Scanner(System.in);
 
         System.out.println("Introduzca cual quier frase:");
         String frase = lu.nextLine();
+        for (int i = frase.length() - 1; i >= 0; i--) {
+            s2 = s2 + frase.charAt(i);
+        }
+        System.out.println("este es el texto: "+frase);
 
-        
-
+        System.out.println("este es el texto al reves: "+s2);
 
     }
 }
