4b6217ff057e0364ffe4f09597f7a5d2092b3e24
Alberto Luna <123046909@UPQ.EDU.MX>
2025-01-28 16:12:40 -0600

    se finalizo programa

--- Practica No.4 programa 1/src/Numeropara.java
+++ Practica No.4 programa 1/src/Numeropara.java
@@ -1,9 +1,24 @@
-Import java.ut
+import java.util.*;
 public class Numeropara {
     public static void main(String[] args) {
 
-        System.out.println("Por alberto luna");
+        /*
+        2. Escribir un programa que pida al usuario un número entero y muestre por
+        pantalla si es par o impar.
+         */
 
+        System.out.println("Por alberto luna");
+        Scanner ln = new Scanner(System.in);
+
+        System.out.print("Ingrese un número : ");
+        int n = ln.nextInt();
+
+        if (n % 2 == 0) {
+
+           System.out.println(n + " es par");
+        }else {
+            System.out.println(n + " es impar");
+        }
 
     }
 }
