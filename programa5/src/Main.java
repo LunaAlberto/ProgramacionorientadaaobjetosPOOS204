c4f0a63b715afad4575856f1a055a2ccd3b21b8d
Alberto Luna <123046909@UPQ.EDU.MX>
2025-01-29 14:55:32 -0600

    se finalizo primer programa del proyecto 5

--- programa5/src/Main.java
+++ programa5/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Por Alberto ");
+
+        /*
+1. Escribir un programa que pida al usuario un número entero positivo y muestre
+por pantalla todos los números impares desde 1 hasta ese número separados
+por comas.
+         */
+        Scanner lu = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero positivo:");
+        int num = lu.nextInt();
+
+        for (int i = 0; i <= num; i++) {
+            if (i % 2!=0) {
+                System.out.print(i+",");
+            }
+        }
+
+
     }
 }
