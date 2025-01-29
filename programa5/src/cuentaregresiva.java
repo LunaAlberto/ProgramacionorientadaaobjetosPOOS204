1f286773e9f20f3889b45ae10bb272e2ddd21e3a
Alberto Luna <123046909@UPQ.EDU.MX>
2025-01-29 15:34:04 -0600

    se creo programa de tabla del 10

--- programa5/src/cuentaregresiva.java
+++ programa5/src/cuentaregresiva.java
@@ -3,8 +3,24 @@ public class cuentaregresiva {
 
     public static void main(String[] args) {
         System.out.println("Por Alberto ");
+       /*
+ 2. Escribir un programa que pida al usuario un número entero positivo y muestre
+por pantalla la cuenta atrás desde ese número hasta cero separados por comas.
+         */
+
+        Scanner lu = new Scanner(System.in);
+
+        System.out.println("ingrese un numero entero positivo:");
+        int numero = lu.nextInt();
+
+ for (int i=numero;i>=0; i--) {
+     System.out.println(i+",");
 
 
-        
+ }
+
+
+
+
     }
 }
---
+++ programa5/src/tabladel10.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+public class tabladel10 {
+    public static void main(String[] args) {
+        System.out.println("Por Alberto ");
+        Scanner input = new Scanner(System.in);
+
+        /*
+        3. Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
+         */
+int num;
+      for(int i=1;i<=10;i++){
+          num = i*10;
+          System.out.println("Ingrese un numero"+i);
+
+
+
+      }
+
+    }
+}
