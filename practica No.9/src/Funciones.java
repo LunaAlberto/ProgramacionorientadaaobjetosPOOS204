42c805c199c019532796468ef47b98e668bd9bc5
Alberto Luna <123046909@UPQ.EDU.MX>
2025-02-17 22:35:47 -0600

    se creo banco

--- practica No.9/src/Funciones.java
+++ practica No.9/src/Funciones.java
@@ -1,50 +1,9 @@
 public class Funciones {
 
-    public String datos;
-    int numerodecuenta;
-    int cuentaatraspasar;
-    double deposito;
-    double retiro;
-    double saldo;
-    String titular;
-    int edad;
-    String nombre;
-    double mostrarsaldo;
 
-    //funciones
+public int numerodecuenta;
+public double saldo;
+public String titular;
 
-    Funciones f = new Funciones();
 
-    public void mosntrarinfo(){
-        System.out.println("Titular:"+titular);
-        System.out.println("numero de cuenta:"+numerodecuenta);
-    }
-    public double mostrarsaldo(){
-        return saldo;
-    }
 
-    public void mostrarretiro(double monto){
-        if(monto<=saldo){
-            saldo-=monto;
-            System.out.println("Saldo actual: "+saldo);
-
-        }else{
-            System.out.println("Saldo insuficiente");
-        }
-    }
-
-    public void mostrardeposito(double monto){
-        saldo+=monto;
-        System.out.println("Saldo actual: "+saldo);
-    }
-
-    public void trasbferencia(Funciones destino,double monto){
-        if(monto<=saldo){
-            saldo-=monto;
-            destino.saldo+=monto;
-            System.out.println("Saldo actual: "+destino.saldo);
-        }else {
-            System.out.println("Saldo insuficiente");
-        }
-    }
-}
