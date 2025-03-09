3edde3c894a08d25efa7d41f2b9acfba665e0af9
Alberto Luna <123046909@UPQ.EDU.MX>
2025-03-09 17:52:03 -0600

    S

--- practicas/src/Main.java
+++ practicas/src/Main.java
@@ -1,5 +1,15 @@
+import javax.swing.JOptionPane;
+import javax.swing.*;
+import javax.swing.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        String opc;
+        int largo ;
+        String caracterespecial;
+        String mayuscula;
+
+
     }
 }
