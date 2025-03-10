68ec011ec885ca378f7d9477ad503058f91794e8
Alberto Luna <123046909@UPQ.EDU.MX>
2025-03-09 19:16:33 -0600

    SD

--- practicas/src/Main.java
+++ practicas/src/Main.java
@@ -1,15 +1,45 @@
 import javax.swing.JOptionPane;
 import javax.swing.*;
-import javax.swing.
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        clasenumero1 class1 = new clasenumero1(8,"","");
 
         String opc;
         int largo ;
         String caracterespecial;
         String mayuscula;
+        String password="";
 
+        do {
+        opc = JOptionPane.showInputDialog("Ingrese una 1 ingresar datos 2 comprobar seguridad y tres salir");
+        switch (opc) {
+            case "1":
+                largo = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el largo:"));
+                caracterespecial = JOptionPane.showInputDialog("Ingrese el caracterespecial:");
+                mayuscula = JOptionPane.showInputDialog("Ingrese la mayuscula:");
 
+                password = class1.generarPassword(largo, caracterespecial, mayuscula);
+                JOptionPane.showMessageDialog(null, password);
+
+                break;
+                case "2":
+                    if (password.isEmpty()) {
+                        JOptionPane.showMessageDialog(null, "Primero genere una contraseña.");
+                    } else {
+                        boolean segura = class1.verificarSeguridad(password);
+                        if (segura) {
+                            JOptionPane.showMessageDialog(null, "Contraseña segura");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Contraseña débil");
+                        }
+                    }
+
+                    break;
+                    default:
+        }
+        }while (!opc.equals("3"));
     }
 }
--- practicas/src/clasenumero1.java
+++ practicas/src/clasenumero1.java
@@ -1,6 +1,82 @@
 import javax.swing.JOptionPane;
 import javax.swing.*;
+import java.util.Random;
 public class clasenumero1 {
+
+    private int largo ;
+
+    public int getLargo() {
+        return largo;
+    }
+
+    public void setLargo(int largo) {
+        this.largo = largo;
+    }
+
+    public String getCaracterespecial() {
+        return caracterespecial;
+    }
+
+    public void setCaracterespecial(String caracterespecial) {
+        this.caracterespecial = caracterespecial;
+    }
+
+    private String caracterespecial;
+
+    public String getMayuscula() {
+        return mayuscula;
+    }
+
+    public void setMayuscula(String mayuscula) {
+        this.mayuscula = mayuscula;
+    }
+
+    private String mayuscula;
+
+    public void larg(int largo){
+        if(largo<=8){
+            this.largo=largo;
+            JOptionPane.showMessageDialog(null, "La larga es: "+largo);
+        }else {
+            JOptionPane.showMessageDialog(null, "No cumpple con lo necesario: "+largo);
+        }
+    }
+
+    public void caracterespecial(String caracterespecial){
+        this.caracterespecial=caracterespecial;
+    }
+
+    public void mayuscula(String mayuscula){
+        this.mayuscula=mayuscula;
+    }
+
+    public clasenumero1(int largo, String caracterespecial, String mayuscula) {
+        this.largo=largo;
+        this.caracterespecial=caracterespecial;
+        this.mayuscula=mayuscula;
+
+    }
+
+    public String generarPassword(int largo, String caracteresEspeciales, String mayusculas) {
+        Random rand = new Random();
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < largo - 2; i++) {
+            char randomChar = (char) (rand.nextInt(26) + 'a');
+            password.append(randomChar);
+        }
+
+        password.insert(rand.nextInt(password.length() + 1), caracteresEspeciales);
+        password.insert(rand.nextInt(password.length() + 1), mayusculas);
+
+        return password.toString();
+    }
+
+    public boolean verificarSeguridad(String password) {
+        return password.length() >= 4 &&
+                password.matches(".*[A-Z].*") &&
+                password.matches(".*[!@#$%^&*()_+\\-=].*");
+    }
 
 
 }
