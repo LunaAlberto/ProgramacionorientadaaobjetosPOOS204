490bd713a47588ea9d4fac096e26470e122f8088
Alberto Luna <123046909@UPQ.EDU.MX>
2025-04-14 14:54:28 -0600

    se creo

--- examen_segundoparcialfinal/src/Main.java
+++ examen_segundoparcialfinal/src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String numero;
+        convertir sub=new
+
+
+       numero = JOptionPane.showInputDialog("Digite um numero: ");
+
+
     }
 }
---
+++ examen_segundoparcialfinal/src/convertir.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+public class convertir {
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String numero;
+
+    public convertir(String numero) {
+        this.numero = numero;
+    }
+
+}
